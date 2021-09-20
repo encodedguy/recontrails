33759ff07a43e6e0347e817613845bcddd2433b9
encodedguy <singhinder1208@gmail.com>
2021-09-20 22:41:05 +0530

    APIUsage() added and working

--- cmd/securitytrails/main.go
+++ cmd/securitytrails/main.go
@@ -17,10 +17,12 @@ func main() {
 
 	s := securitytrails.New(apiKey)
 	info, err := s.APIInfo()
+	usage, err := s.APIUsage()
 
 	if err != nil{
 		log.Panicln(err)
 	}
 
-	fmt.Printf("APIKey Valid: %t", info.Success)
+	fmt.Printf("APIKey Valid: %t\n", info.Success)
+	fmt.Printf("API Allowed Credits: %d\nAPI Used Credits: %d\n", usage.AllowedMonthlyUsage, usage.CurrentMonthlyUsage)
 }
